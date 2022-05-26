0812e517bd8b2d2f100538a279af98156d5abd7a
Justin Jia <jiajia1015@gmail.com>
2022-05-26 15:52:48 +0800

    Update attention_demo_utils.py

--- utils/attention_demo_utils.py
+++ utils/attention_demo_utils.py
@@ -260,3 +260,85 @@ def train_step(inp, targ, targ_lang, encoder, decoder, enc_hidden, optimizer, ba
     optimizer.apply_gradients(zip(gradients, variables))
 
     return batch_loss
+
+
+def evaluate(sentence, inp_tensor, inp_lang, targ_tensor, targ_lang, encoder, decoder):
+    """Translate a sentence
+    Args:
+        sentence: the test sentence        
+    """
+    max_length_inp = max_length(inp_tensor)
+    max_length_targ = max_length(targ_tensor)
+    # max_length_targ 38, max_length_inp 64
+    attention_plot = np.zeros((max_length_targ, max_length_inp))
+
+    sentence = preprocess_chinese(sentence)
+
+    # convert each word to the index in the test sentence
+    inputs = [inp_lang.word_index[i] for i in sentence.split(' ')]
+    inputs = tf.keras.preprocessing.sequence.pad_sequences([inputs],
+                                                           maxlen=max_length_inp,
+                                                           padding='post')
+
+    inputs = tf.convert_to_tensor(inputs)
+
+    result = ''
+
+    # hidden shape == (1, 1024)
+    hidden = [tf.zeros((1, units))]
+    
+    # enc out shape == (1, max_length_inp, 1024) -> (1, 46, 1024)
+    # enc hidden shape == (1, 1024)
+    enc_out, enc_hidden = encoder(inputs, hidden)
+
+    dec_hidden = enc_hidden
+    dec_input = tf.expand_dims([targ_lang.word_index['<start>']], 0)
+
+    for t in range(max_length_targ):
+        predictions, dec_hidden, attention_weights = decoder(dec_input, dec_hidden, enc_out)
+        
+        # storing the attention weigths to plot later on
+        attention_weights = tf.reshape(attention_weights, (-1, ))
+        attention_plot[t] = attention_weights.numpy()
+
+        # get the index which has the highest probability
+        predicted_id = tf.argmax(predictions[0]).numpy()
+        #  convert the index to the word
+        result += targ_lang.index_word[predicted_id] + ' '
+
+        # when the decoder predicts the end, stop prediction
+        if targ_lang.index_word[predicted_id] == '<end>':
+            return result, sentence, attention_plot
+
+        # the predicted id is fed back into the model
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    return result, sentence, attention_plot
+
+
+# function for plotting the attention weights
+def plot_attention(attention, sentence, predicted_sentence):
+    # you need to change the fname based on your system, and the Chinese can be displayed in the plot
+    font = FontProperties(fname=r"./TaipeiSansTCBeta-Regular.ttf", size=14)
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.matshow(attention, cmap='viridis')
+
+    fontdict = {'fontsize': 14}
+
+    # set the x-tick/y-tick labels with list of string labels
+    ax.set_xticklabels([''] + sentence, fontdict=fontdict, fontproperties=font)
+    ax.set_yticklabels([''] + predicted_sentence, fontdict=fontdict, fontproperties=font)
+
+    # set tick locators
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+    plt.show()
+
+def translate(sentence):
+    result, sentence, attention_plot = evaluate(sentence, input_tensor, inp_lang, target_tensor, targ_lang, encoder, decoder)
+    print('Input: %s' % (sentence))
+    print('Predicted translation: {}'.format(result))
+
+    attention_plot = attention_plot[:len(result.split(' ')), :len(sentence.split(' '))]
+    plot_attention(attention_plot, sentence.split(' '), result.split(' '))
